9b1e94ecc1e7b285ef61fc886b7458838dc31810
lazyhat <sevapetrov2016p@gmail.com>
2024-12-26 17:24:01 +0300

    deploy fix

--- src/main/kotlin/ru/jetlabs/ts/tourdataservice/TourDataServiceApplication.kt
+++ src/main/kotlin/ru/jetlabs/ts/tourdataservice/TourDataServiceApplication.kt
@@ -3,8 +3,12 @@ package ru.jetlabs.ts.tourdataservice
 import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @ImportAutoConfiguration(ExposedAutoConfiguration::class)
 class TourDataServiceApplication
 
+fun main(args: Array<String>) {
+    runApplication<TourDataServiceApplication>(*args)
+}
