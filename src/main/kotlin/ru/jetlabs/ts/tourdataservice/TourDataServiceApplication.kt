7cad60d10d618cf4e815afdb285bb071203212cf
lazyhat <sevapetrov2016p@gmail.com>
2024-12-26 17:15:22 +0300

    deploy

--- src/main/kotlin/ru/jetlabs/ts/tourdataservice/TourDataServiceApplication.kt
+++ src/main/kotlin/ru/jetlabs/ts/tourdataservice/TourDataServiceApplication.kt
@@ -1,7 +1,10 @@
 package ru.jetlabs.ts.tourdataservice
 
+import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
+@ImportAutoConfiguration(ExposedAutoConfiguration::class)
 class TourDataServiceApplication
 
