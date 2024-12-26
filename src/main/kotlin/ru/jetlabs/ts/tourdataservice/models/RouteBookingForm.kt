6fff38702a12ae4d6407b0ea0d449b5a22dad410
lazyhat <sevapetrov2016p@gmail.com>
2024-12-26 13:12:50 +0300

    reduce logic

--- src/main/kotlin/ru/jetlabs/ts/tourdataservice/models/RouteBookingForm.kt
+++
@@ -1,7 +0,0 @@
-package ru.jetlabs.ts.tourdataservice.models
-
-data class RouteBookingForm(
-    val ticketId: Long,
-    val routeId: Long,
-    val personCount: Int
-)
--- src/main/kotlin/ru/jetlabs/ts/tourdataservice/models/results/GetBookingDataByTicketId.kt
+++
@@ -1,7 +0,0 @@
-package ru.jetlabs.ts.tourdataservice.models.results
-
-import ru.jetlabs.ts.tourdataservice.models.RouteBooking
-
-sealed interface GetBookingDataByTicketId {
-    data class Success(val data: List<RouteBooking>) : GetBookingDataByTicketId
-}
